a50ae416c7df07ec7d0f60e363b004b7b44a91ab
SkyXcripto {dev} <producer@skynance.net>
2021-11-17 14:42:23 -0300

    deploy

---
+++ static/js/4.3927f249.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonppassive=this.webpackJsonppassive||[]).push([[4],{700:function(s,p){}}]);
+//# sourceMappingURL=4.3927f249.chunk.js.map
--- static/js/4.fe9a76ce.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonppassive=this.webpackJsonppassive||[]).push([[4],{698:function(s,p){}}]);
-//# sourceMappingURL=4.fe9a76ce.chunk.js.map
