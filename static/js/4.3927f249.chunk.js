e9f4d94ad29087735896f122d5f8e3bd14dbb4f9
SkyXcripto {dev} <producer@skynance.net>
2021-11-18 10:58:00 -0300

    update

--- static/js/4.3927f249.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonppassive=this.webpackJsonppassive||[]).push([[4],{700:function(s,p){}}]);
-//# sourceMappingURL=4.3927f249.chunk.js.map
---
+++ static/js/4.8728f30f.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonppassive=this.webpackJsonppassive||[]).push([[4],{702:function(s,p){}}]);
+//# sourceMappingURL=4.8728f30f.chunk.js.map
