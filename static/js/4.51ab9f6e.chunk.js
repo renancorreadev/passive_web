54e3b7659de4b70bb4dab9d4af390349beb9b8de
SkyXcripto {dev} <producer@skynance.net>
2021-11-27 11:09:09 -0300

    update mobile fix

---
+++ static/js/4.51ab9f6e.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonppassive=this.webpackJsonppassive||[]).push([[4],{696:function(s,p){}}]);
+//# sourceMappingURL=4.51ab9f6e.chunk.js.map
--- static/js/4.c5a5eb7f.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonppassive=this.webpackJsonppassive||[]).push([[4],{672:function(s,p){}}]);
-//# sourceMappingURL=4.c5a5eb7f.chunk.js.map
